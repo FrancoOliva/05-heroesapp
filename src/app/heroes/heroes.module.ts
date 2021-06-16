c67de864cbacb33c000e2f3c60f91e8aaf1779eb
Franco Oliva <franco.oliva1991@gmail.com>
2021-06-16 09:56:42 -0300

    s13 continuar con la clase 189

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -10,6 +10,7 @@ import { HeroeComponent } from './pages/heroe/heroe.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
 import { HeroesRoutingModule } from './heroes-routing.module';
+import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
 
 
 
@@ -19,7 +20,8 @@ import { HeroesRoutingModule } from './heroes-routing.module';
     BuscarComponent,
     HeroeComponent,
     HomeComponent,
-    ListadoComponent
+    ListadoComponent,
+    HeroeTarjetaComponent
   ],
   imports: [
     CommonModule,
