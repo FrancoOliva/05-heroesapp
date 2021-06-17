8568fd56be61fdd95307edebf0260c038b16ed1f
Franco Oliva <franco.oliva1991@gmail.com>
2021-06-17 18:04:32 -0300

    fin sección 13

--- src/app/heroes/pages/buscar/buscar.component.ts
+++ src/app/heroes/pages/buscar/buscar.component.ts
@@ -14,7 +14,7 @@ export class BuscarComponent implements OnInit {
   termino: string = '';
   heroes: Heroe[] = [];
 
-  heroeSeleccionado!: Heroe;
+  heroeSeleccionado: Heroe | undefined;
 
   constructor( private heroesService: HeroesService) { }
 
@@ -23,18 +23,30 @@ export class BuscarComponent implements OnInit {
 
   buscando(){
 
-    this.heroesService.getSugerencias( this.termino )
+    this.heroesService.getSugerencias( this.termino.trim() )
     .subscribe( heroes => this.heroes = heroes);
 
   }
 
   opcionSeleccionada( evento:MatAutocompleteSelectedEvent ){
+
+    // si no tienen ningún valor
+    if(!evento.option.value){
+      this.heroeSeleccionado = undefined;
+      console.log('limpiar json');
+      return;
+    }
+
+
     const heroe: Heroe = evento.option.value;
+    
     this.termino = heroe.superhero;
 
-    // seleccionamos al héroe de nuestra búsqueda y traemos los datos de la api de ESE HÉROE
-    this.heroesService.getHeroeId( heroe.id! )
-    .subscribe( heroe => this.heroeSeleccionado = heroe );
+      // seleccionamos al héroe de nuestra búsqueda y traemos los datos de la api de ESE HÉROE
+      this.heroesService.getHeroeId( heroe.id! )
+      .subscribe( heroe => this.heroeSeleccionado = heroe );
+
+    
   }
 
 }
